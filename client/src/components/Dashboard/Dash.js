31542b70bb202c3978df7163df484e9daa0bf400
iamtheluckyest <annalisa.alden@gmail.com>
2017-10-04 14:34:16 -0400

    set up custom bootstrap files

--- client/src/components/Dashboard/Dash.js
+++ client/src/components/Dashboard/Dash.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export const Dash = (props) => 
-    
+    <h1>hey</h1>
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '@customstyles/bootstrap/build/index.css';
+
+import './index.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
