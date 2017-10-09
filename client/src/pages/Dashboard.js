655197a53bbfb2ca52588a543f7a57f9b6c73057
iamtheluckyest <annalisa.alden@gmail.com>
2017-10-08 22:25:20 -0400

    dashboard component reloads when item is deleted

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,23 +2,21 @@ import React, {Component} from "react";
 import {Container} from "reactstrap"
 import {Dash} from "../components/Dashboard";
 import Auth from "../Auth.js";
+import API from "../utils/API";
 import axios from "axios";
 
 export class Dashboard extends Component {
-
-    constructor(props){
-        super(props);
-
-        this.state = {
-            user : undefined
-        }
+    state = {
+        user : undefined,
+        schools: []
     }
 
-    /**
-     * This method will be executed after initial rendering.
-     */
     componentDidMount() {
         document.title = "Dashboard"
+        this.getUser();
+    }
+
+    getUser =() => {
         let that = this;
         axios({
             url : '/user',
@@ -30,14 +28,52 @@ export class Dashboard extends Component {
             console.log(res);
             that.setState({
                 user : res.data
-            });
+            }, () => that.updateSchoolList());
         }).catch(function(err){
             console.log(err);
             that.setState({
                 user : undefined
-            })
+            } )
         });
     }
+
+    updateSchoolList = () => {
+        // Stores only the school data
+        this.setState({schools: []}, 
+            this.state.user.colleges.forEach( (school, index) => {
+                this.getSchoolInfo(school.apiId, index);
+            })
+        )
+    }
+
+    getSchoolInfo = (apiId, index) => {
+        let authKey = "S9HBrRONDcEoZvlEQkn5ucv5bmWnMoRT5sjaWIJ8";
+        let queryUrl = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=" + authKey + "&id=" + apiId;
+        return (
+            API.getSchoolById(queryUrl)
+                .then(res => {
+                    let arr = this.state.schools;
+                    arr[index] = res.data.results[0];
+                    this.setState({schools: arr});
+                })
+                .catch(err => console.log(err))
+        )
+    }
+
+    deleteSchool = collegeId => {
+        API.deleteSchool({
+            method: "delete",
+            url: "/user/college",
+            headers: {
+                'Authorization' : `bearer ${Auth.getToken()}`
+            },
+            data: {
+                collegeId: collegeId
+            }
+        }).then( () => this.getUser() )
+        .catch(err=> console.log(err));
+    }
+    
 
     render() {
         console.log("state is: ");
@@ -47,7 +83,7 @@ export class Dashboard extends Component {
                 {
                     this.state.user 
                     ?
-                    <Dash user={this.state.user}/>   
+                    <Dash user={this.state.user} schools={this.state.schools} deleteSchool={this.deleteSchool}/>   
                     : 
                     <p style={{textAlign: "center", marginTop: "30px"}}>Sorry, you must be logged in to access this content.</p>
                 }
