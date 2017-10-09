c5ebbf72227b695600b4a512966216d99936a140
iamtheluckyest <annalisa.alden@gmail.com>
2017-10-09 09:54:33 -0400

    change to noMatch

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -3,7 +3,7 @@ import {Title} from "../components/Layout";
 
 export const NoMatch = props =>
     <div>
-        <Title title="No Match"/>
+        <Title title="Page not found"/>
         <p style={{textAlign: "center", marginTop: "30px"}}>
             Sorry, no page exists with that path name.
         </p>
